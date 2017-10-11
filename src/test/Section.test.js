e4ebde5d9716684ed93147ecd44ae90cb3c6014f
Unai Motriko <unaimotriko@gmail.com>
2017-10-11 11:41:47 +0100

    Delete Section.test.js

--- src/test/Section.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Section from '../components/Section';
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Section />, div);
-});
