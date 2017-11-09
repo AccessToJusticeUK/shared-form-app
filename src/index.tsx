b833d703cf4db922d79b45fb25fc5962f70b43fe
Stephanie Siaw <ssiaw@thoughtworks.com>
2017-11-09 12:00:16 +0000

    TECH - SS - export preamble in index to see if it can be used in insurance checker repo as npm package

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './styling/cjc-bootstrap-default.css';
 import './styling/styles.scss';
-
 import { App } from './App';
+
+export { Preamble } from './components/Preamble';
 
 ReactDOM.render(
     <App />,
