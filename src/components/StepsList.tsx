a8feb114effc43ed35c1115bfa2b42b611ab6e3c
Stephanie Siaw <ssiaw@thoughtworks.com>
2017-11-15 11:19:23 +0000

    [MISC] - SS - Fix button styles and index class name

--- src/components/StepsList.tsx
+++ src/components/StepsList.tsx
@@ -30,7 +30,7 @@ export class StepsList extends React.Component<StepsListProps, StepsListState> {
                     <p><span className="text-large">You may already have legal insurance that you did not know about!</span></p>
                     <p>This page will walk you through the most likely sources of LEI (Legal Expenses Insurance) that
                     are often bundled with your existing financial products.</p>
-                    <p>Having funds to cover your legal expenses will ensure your problem gets priority and resolved quickly</p>
+                    <p>Having funds to cover your legal expenses will ensure your problem gets priority and resolved quickly.</p>
                 </PagePreambleSection>
                 <PagePreambleSection>
                     <p>Before you start it may be helpful to have any paperwork regarding your bank accounts, credit cards and insurance to hand.</p>
